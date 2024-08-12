861c63eb42a0cabaabbb6870e168e6faf816b0af
Andrea Casaluci <andrea.casaluci@orbyta.it>
2024-08-12 11:33:54 +0200

    chore: project initialization

---
+++ src/main/java/com/andrea/reactive/ReactiveSatellitesApp.java
@@ -0,0 +1,13 @@
+package com.andrea.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSatellitesApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSatellitesApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrea/reactive/ReactiveSatellitesAppTests.java
@@ -0,0 +1,13 @@
+package com.andrea.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSatellitesAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
