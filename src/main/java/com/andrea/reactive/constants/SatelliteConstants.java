8b129ba2a90bf4cefac4e8bf8cee5ba15844ad85
Andrea Casaluci <andrea.casaluci@orbyta.it>
2024-08-12 18:08:39 +0200

    fix: fixed fetch max size value constant

--- src/main/java/com/andrea/reactive/constants/SatelliteConstants.java
+++ src/main/java/com/andrea/reactive/constants/SatelliteConstants.java
@@ -12,7 +12,7 @@ public class SatelliteConstants {
     public static final String FETCH_SIZE_PARAM_NAME = "size";
     public static final String FETCH_DEFAULT_SIZE_VALUE = "10";
     public static final int FETCH_MIN_SIZE_VALUE = 1;
-    public static final int FETCH_MAX_SIZE_VALUE = 1;
+    public static final int FETCH_MAX_SIZE_VALUE = 100;
 
     public static final String EXTERNAL_API_PAGE_PARAMETER_NAME = "page";
     public static final int EXTERNAL_API_DEFAULT_PAGE_VALUE = 1;
